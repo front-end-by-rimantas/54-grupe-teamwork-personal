7ecdbb6e9b11751cb4a8c8f239f3944ced326066
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-15 21:21:33 +0300

    transition

--- js/components/header.js
+++ js/components/header.js
@@ -8,7 +8,28 @@ export function header() {
     { text: "contacts", href: "/contacts/" },
   ];
 
-  const HTML = `
+  const lp = location.pathname;
+  const currentPage = lp.length > 1 && lp.at(-1) === "/" ? lp.slice(0, -1) : lp;
+  let linksHTML = "";
+
+  for (const link of menu) {
+    let activePage = "";
+    let projectName = "";
+    if (projectName + link.href === location.pathname) {
+      activePage = "active";
+    }
+    linksHTML += `<a class='${activePage}' href=".${link.href}" target="_blanc" > ${link.text} </a>`;
+  }
+  linksHTML += `<div class="dropdown">
+    <button class="dropbtn">Blog
+      <i class="fa fa-angle-down"></i>
+    </button>
+    <div class="dropdown-content nav-links">
+      <a href="#">Blog Home</a>
+      <a href="#">Blog Single</a>
+    </div>`;
+
+  let HTML = `
         <header class="container">
       <div class="logo col-2">
         <img src="./img/logo.webp" alt="Logo" />
@@ -19,5 +40,3 @@ export function header() {
     </header>`;
   document.body.insertAdjacentHTML("beforeend", HTML);
 }
-
-// <a href="#">Home</a>
