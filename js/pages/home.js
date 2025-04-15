2fdc521e9866820a753c9b6e03275d0b81a3395c
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-15 15:22:30 +0300

    layout

---
+++ js/pages/home.js
@@ -0,0 +1,35 @@
+import { header } from "../components/header.js";
+import { hero } from "../components/hero.js";
+import { sectionTitle } from "../components/section-title.js";
+
+/* header */
+header();
+/* hero this is me */
+hero();
+
+/* about me */
+
+/* My Offered Services daryti section-title.css */
+sectionTitle();
+
+/* 2 x 3 row */
+
+/* count animation */
+
+/* Our Latest Featured Projects same as My Offered */
+
+/* 6 pics filter */
+
+/* Feedback */
+
+/* Choose your plan same as My Offered */
+
+/* plan 4 column + hower*/
+
+/* Latest posts from our blog - section-title.css */
+
+/* 3 column with pic  */
+
+/* subfooter  */
+
+/* footer */
