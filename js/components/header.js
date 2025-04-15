1dc1cb9afa4594803b431d1ed4862ffbd195dd11
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-15 16:52:52 +0300

    header

--- js/components/header.js
+++ js/components/header.js
@@ -1,7 +1,23 @@
 export function header() {
+  const menu = [
+    { text: "home", href: "/" },
+    { text: "about", href: "/about/" },
+    { text: "services", href: "/services/" },
+    { text: "portfolio", href: "/portfolio/" },
+    { text: "pricing", href: "/pricing/" },
+    { text: "contacts", href: "/contacts/" },
+  ];
+
   const HTML = `
-        <header>
-            HEADER CONTENT
-        </header>`;
+        <header class="container">
+      <div class="logo col-2">
+        <img src="./img/logo.webp" alt="Logo" />
+      </div>
+      <nav class="nav-links col-8 m-2">  
+      ${linksHTML}         
+      </nav>
+    </header>`;
   document.body.insertAdjacentHTML("beforeend", HTML);
 }
+
+// <a href="#">Home</a>
