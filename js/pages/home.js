b4db3b67d22a6384516fd40c7e4dc4e924becb3d
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-16 16:34:13 +0300

    footer js css

--- js/pages/home.js
+++ js/pages/home.js
@@ -12,7 +12,10 @@ hero();
 /* about me */
 
 /* My Offered Services daryti section-title.css */
-sectionTitle("My Offered Services", "At about this time of year, some months after New Year's resolutions have been made and kept, or made and neglected.");
+sectionTitle(
+  "My Offered Services",
+  "At about this time of year, some months after New Year's resolutions have been made and kept, or made and neglected."
+);
 
 /* 2 x 3 row */
 singleServices();
@@ -24,23 +27,28 @@ sectionTitle("Our Latest Featured Projects", "Who are in extremely love with eco
 /* 6 pics filter */
 
 /* Feedback */
-sectionTitle("Client's Feedback About Me", "It is very easy to start smoking but it is an uphill task to quit it. Ask any chain smoker or even a person.");
+sectionTitle(
+  "Client's Feedback About Me",
+  "It is very easy to start smoking but it is an uphill task to quit it. Ask any chain smoker or even a person."
+);
 /* Choose your plan same as My Offered */
 sectionTitle("Choose Your Plan", "When someone does something that they know that they shouldn't do, did they.");
 /* plan 4 column + hower*/
 
-
 /* Latest posts from our blog - section-title.css */
-sectionTitle("Latest Posts From Our Blog", "You may be a skillful, effective employer but if you don't trust your personnel and the opposite, then the chances of improving and expanding the business.");
+sectionTitle(
+  "Latest Posts From Our Blog",
+  "You may be a skillful, effective employer but if you don't trust your personnel and the opposite, then the chances of improving and expanding the business."
+);
 
 /* Latest posts title - section-title.css */
 
 /* Latest post content */
 latestPosts();
 
-
 /* 3 column with pic  */
 
 /* subfooter  */
 
 /* footer */
+footer();
