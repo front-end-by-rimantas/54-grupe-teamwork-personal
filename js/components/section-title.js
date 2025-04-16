f9e07cc7055538e05bfa875a7dec45a9068b0f37
GintareLa <lapiene.g@gmail.com>
2025-04-16 10:38:31 +0300

    section-title

--- js/components/section-title.js
+++ js/components/section-title.js
@@ -1,11 +1,22 @@
-export function sectionTitle() {
+export function sectionTitle(title, description) {
   const HTML = `
-        <section class="container">
-            <div class="row">
+        <section class="container section-container">
+        <div class="row">
                 <div class="col-12">
-                    HERO CONTENT
+                <div class="section-title">
+                    <h1>${title}</h1>
+                    <p class="col-12">${description}</p>
                 </div>
             </div>
         </section>`;
   document.body.insertAdjacentHTML("beforeend", HTML);
 }
+
+
+// sectionTitle("My Qualifications", "Who are in extremely love with eco friendly system.");
+// sectionTitle("Frequently Asked Questions", "When someone does something that they know that they shouldn't do, did they really have a choice. Maybe what I mean to say is did they really have a chance. You can take two people.");
+
+
+
+
+
