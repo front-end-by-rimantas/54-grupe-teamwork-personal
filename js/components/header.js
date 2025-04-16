a4a8f0c30290b50107425860b5436f3423a64650
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-16 15:41:05 +0300

    header no response

--- js/components/header.js
+++ js/components/header.js
@@ -21,14 +21,15 @@ export function header() {
       activePage = "active";
     }
     if (link.drop === true) {
-      linksHTML += `<div class="dropdown">
-    <button class="dropbtn">${link.text}
-      <i class="fa fa-angle-down"></i>
-    </button>
-    <div class="dropdown-content nav-links">
-      <a href="#">Blog Home</a>
-      <a href="#">Blog Single</a>
-    </div>
+      linksHTML += `
+    <div class="dropdown">
+      <button class="dropbtn">${link.text}
+        <i class="fa fa-angle-down"></i>
+      </button>
+      <div class="dropdown-content nav-links">
+        <a href="#">Blog Home</a>
+        <a href="#">Blog Single</a>
+      </div>
     </div>`;
     } else {
       linksHTML += `<a class='${activePage}' href=".${link.href}" target="_blanc" > ${link.text} </a>`;
@@ -36,15 +37,25 @@ export function header() {
   }
 
   let HTML = `
-        <header class="container">
-      <div class="logo col-2">
+    <header class="">
+      <div class="logo">
         <img src="./img/logo.webp" alt="Logo" />
       </div>
-      <nav class="nav-links col-8 m-2">  
-      ${linksHTML}         
+      <nav class="nav-links">  
+        ${linksHTML}         
       </nav>
     </header>`;
   document.body.insertAdjacentHTML("beforeend", HTML);
+
+  const headerEl = document.querySelector("header");
+
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 150) {
+      headerEl.classList.add("shadow");
+    } else {
+      headerEl.classList.remove("shadow");
+    }
+  });
 }
 
 // dropdown - content; hower addEventListener
