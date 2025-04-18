d4bd5b3b7e0370e8b79d643dff1bb2e02d412b45
GintareLa <lapiene.g@gmail.com>
2025-04-18 09:34:15 +0300

    sub-footer finish

--- js/components/sub-footer.js
+++ js/components/sub-footer.js
@@ -1,20 +1,3 @@
-// export function subFooter () {
-// const HTML = `
-// <section class="container sub-footer">
-//     <div class="row purple-row">
-//         <div class="col-12 purple-content">
-//             <a href="#">
-//                 <img class="img-slide" src="../img/l2.png.webp" alt="creative">
-//                 <img class="img-slide" src="../img/l3.png.webp" alt="premium labels anchor">
-//                 <img class="img-slide" src="../img/l4.webp" alt="premium labels">
-//                 <img class="img-slide" src="../img/l5.png" alt="premium mountains">
-//                 <img class="img-slide" src="../img/l1.png" alt="premium labels crown">
-//             </a>
-//         </div>
-//     </div>
-// </section>`;
-// document.body.insertAdjacentHTML("beforeend", HTML)
-// }
 
     const subFooterImg = [
         {
