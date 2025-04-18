e758111afe55d3773220ffb7b01a11957ef86447
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-04-18 13:24:01 +0300

    hero and about me section moved to js

---
+++ js/components/about-me.js
@@ -0,0 +1,14 @@
+export function aboutMe() {
+    const HTML = `
+         <section class="about-me-section">
+      <div class="content">
+          <h1 class="topic">ABOUT ME</h1>
+          <h2 class="personal-details">PERSONAL DETAILS</h2>
+          <p class="hero-text">Here, I focus on a range of items and features that we use in life without giving them a second thought. such as Coca Cola. Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          <button class="btn">VIEW FULL DETAILS</button>
+      </div>
+      <img class="img" src="./img/about-img.webp" alt="about me">
+</section>`;
+    document.body.insertAdjacentHTML("beforeend", HTML);
+  }
+  
