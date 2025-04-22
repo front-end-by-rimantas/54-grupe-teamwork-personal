88bbedeb72265c9fb75bffd1836b72bb300b83fb
Silvija96 <silvija.barkauskaite064@gmail.com>
2025-04-22 11:19:32 +0300

    blue button link

--- js/components/about-me.js
+++ js/components/about-me.js
@@ -5,7 +5,7 @@ export function aboutMe() {
           <h1 class="topic">ABOUT ME</h1>
           <h2 class="personal-details">PERSONAL DETAILS</h2>
           <p class="hero-text">Here, I focus on a range of items and features that we use in life without giving them a second thought. such as Coca Cola. Dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-          <button class="btn">VIEW FULL DETAILS</button>
+          <a href="#" class="btn">VIEW FULL DETAILS</a>
       </div>
       <img class="img" src="./img/about-img.webp" alt="about me">
 </section>`;
--- js/components/hero.js
+++ js/components/hero.js
@@ -5,7 +5,7 @@ export function hero() {
           <h6 class="topic">THIS IS ME</h6>
           <h1 class="name"> PHILIP GILBERT</h1> 
           <p class="hero-text">You will begin to realise why this exercise is called the Dickens Pattern with reference to the ghost showing Scrooge some different futures.</p>
-          <button class="btn">DISCOVER NOW</button>
+          <a href="#" class="btn">DISCOVER NOW</a>
       </div>
       <img class="img" src="./img/hero-img.webp" alt="Philip Gilbert">
 </section>`;
