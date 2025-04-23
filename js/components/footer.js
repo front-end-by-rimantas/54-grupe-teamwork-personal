1cb00ad249cb8a2a06fa27fea1819cb4655c1e53
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-23 11:28:09 +0300

    container column html filled

--- js/components/footer.js
+++ js/components/footer.js
@@ -25,7 +25,7 @@ export function footer() {
           <p>Stay updated with our latest trends</p>
           <div class="input-div col-12">
             <input class="input-email" type="email" placeholder="Enter email address" name="email" />
-            <div class="input-arrow"><i class="fa fa-arrow-right"></i></div>
+            <button type="submit" class="input-arrow"><i class="fa fa-arrow-right"></i></button>
           </div>
           <p class="submitting">Submitting...</p>
         </div> 
