f2641dec1cc471eaae09e54ef3733e7f2d79dd9f
KristinaKripke <kristina.kripke@gmail.com>
2025-04-25 16:26:39 +0300

    html structure

---
+++ js/pages/contacts.js
@@ -0,0 +1,11 @@
+/*import { header } from "../components/header.js";
+import { blueTitleColumn } from "../components/blue-title-column.js";
+
+
+import { footer } from "../components/footer.js";
+
+
+
+header();
+blueTitleColumn('Contact Us', 'Contact Us', './contacts/index.html')
+footer(); */
