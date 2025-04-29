ab5ce736d419ac5467679d4ccf8be6663fc39583
Rokas Imelinskas <rokas.imelinskas@gmail.com>
2025-04-29 09:05:05 +0300

    free text about

--- js/pages/about.js
+++ js/pages/about.js
@@ -1,10 +1,12 @@
 import { header } from "../components/header.js";
 import { blueTitleColumn } from "../components/blue-title-column.js";
+import { aboutFreeText } from "../components/about-free-text.js";
 import { aboutTools } from "../components/about-tools.js";
 
 import { footer } from "../components/footer.js";
 
 header();
 blueTitleColumn("About Me", "About Me", "./about/index.html");
+aboutFreeText();
 aboutTools();
 footer();
